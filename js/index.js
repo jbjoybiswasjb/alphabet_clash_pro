d3b056ce1243724e36e485a8558ab4e2930b2b6e
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-03-02 17:35:34 +0600

    Generated a random alphabet.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,14 @@
 //     playGround.classList.remove('hidden');
 // }
 
+function continuePlay() {
+    // Step 1: generate a random alphabet.
+    const alphabet = getRandomAlphabet();
+    console.log(alphabet);
+}
+
 function play() {
     classAdd('alphabet_clash_home');
     classRemove('alphabet_clash_playground');
+    continuePlay();
 }
--- js/utility.js
+++ js/utility.js
@@ -1,9 +1,21 @@
-function classAdd(elementId){
+function classAdd(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function classRemove(elementId){
+function classRemove(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+
+function getRandomAlphabet() {
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    
+    const randomNumber = Math.random();
+    const limitRandomNumber = Math.random() * 25;
+    const removeFloatingPointOfRandomNumber = Math.round(limitRandomNumber);
+
+    const randomAlphabet = alphabets[removeFloatingPointOfRandomNumber];
+    return randomAlphabet;
 }
