64bb9d198facc6c62df5003ef71d7a6ee9334355
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-03-02 13:19:32 +0600

    added utility functions with js file.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+// function play() {
+//     const homeSection = document.getElementById('alphabet_clash_home');
+//     homeSection.classList.add('hidden');
+
+//     const playGround = document.getElementById('alphabet_clash_playground');
+//     playGround.classList.remove('hidden');
+// }
+
 function play() {
-    console.log('play.');
+    classAdd('alphabet_clash_home');
+    classRemove('alphabet_clash_playground');
 }
